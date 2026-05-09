ac631d0fecd3b308425bc4e557ee8c4c9189378b
m0n1shgupta <2006monishkumar@gmail.com>
2026-05-09 21:03:19 +0530

    feat: add async support and error handling to scraper

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from requests.exceptions import HTTPError, ConnectionError, Timeout
 from scraper import scrape_text
 from schema import ScrapeRequest
 
 app = FastAPI()
 
 @app.post("/scrape")
-def scrape(data: ScrapeRequest):
+async def scrape(data: ScrapeRequest):
 
-    text = scrape_text(data.url)
+    try:
+        text = await scrape_text(str(data.url))
+        return {
+            "url": str(data.url),
+            "text": text
+        }
 
-    return {
-        "url": data.url,
-        "text": text
-    }
+    except Timeout:
+        raise HTTPException(
+            status_code=status.HTTP_408_REQUEST_TIMEOUT, detail = "Request Time Out"
+        )
+    
+    except ConnectionError:
+        raise HTTPException(
+            status_code=status.HTTP_502_BAD_GATEWAY, detail = "Could not connect to URL"
+        )
+    
+    except HTTPError as e:
+        raise HTTPException(
+            status_code=e.response.status_code, detail = str(e)
+        )
+    
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=f"Unexpected Error: {str(e)}")
--- scraper.py
+++ scraper.py
@@ -1,15 +1,18 @@
-import requests
+import httpx
 from bs4 import BeautifulSoup
 
 # fetch Page -> parse HTML -> strip junk tags -> return clean plain text
 
-def scrape_text(url : str):
+async def scrape_text(url : str):
     #setting user agent to mozilla so websites think that the request is coming from real browser
     header = {
         "User-Agent": "Mozilla/5.0"
     }
     #set header and make the request
-    response = requests.get(url, headers = header)
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers = header, timeout=10)
+        response.raise_for_status()
     #parse the HTML
     soup = BeautifulSoup(response.text, "html.parser")
     #Remove noise tags
